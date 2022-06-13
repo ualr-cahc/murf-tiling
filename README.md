a15296e2644d7b2f6809473b9969cb79b1b46019
triley <triley@ualr.edu>
2022-06-13 16:02:08 -0500

    cleaned code, timed tile levels, reorg notebooks

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # murf-tiling
+
+python 3.10
+
--- tileTools/translate2bytes.py
+++ tileTools/translate2bytes.py
@@ -1,7 +1,9 @@
 import os
 from osgeo.gdal import GDT_Byte, Translate
+from tileTools.calcTime import calcTime
 
-def translate2bytes(filepaths: list[str], output_dir: str):
+@calcTime
+def translate2bytes(input_filepaths: list[str], output_dir: str):
 	"""Input a list of .tif filepaths and an output directory, returns list of errors.
 	Checks for files in output directory and translates those not yet present."""
 	errors = []
@@ -9,9 +11,10 @@ def translate2bytes(filepaths: list[str], output_dir: str):
 	if not os.path.isdir(output_dir):
 		os.mkdir(output_dir)
 	translated_files = [os.path.join(output_dir, file) for file in os.listdir(output_dir)]
-	if not isinstance(filepaths, list):
-		raise ValueError("filepaths variable must be a list of filepaths")
-	for filepath in filepaths:
+	if not isinstance(input_filepaths, list):
+		print(input_filepaths)
+		raise ValueError("input_filepaths variable must be a list of filepaths")
+	for filepath in input_filepaths:
 		filename = os.path.split(filepath)[-1]
 		translated_filepath = os.path.join(output_dir, filename)
 		if translated_filepath not in translated_files:
